0f6b988397f0481090abcaad8dee647a8cea1477
VanIT1706 <nguyenquangvan172@gmail.com>
2023-09-06 11:11:31 +0700

    ToolSetTimeShutdown

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ToolHenGio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numGio = new System.Windows.Forms.NumericUpDown();
+            this.numPhut = new System.Windows.Forms.NumericUpDown();
+            this.numGiay = new System.Windows.Forms.NumericUpDown();
+            this.btnShutDown = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numGio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPhut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGiay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numGio
+            // 
+            this.numGio.Location = new System.Drawing.Point(33, 20);
+            this.numGio.Name = "numGio";
+            this.numGio.Size = new System.Drawing.Size(65, 22);
+            this.numGio.TabIndex = 0;
+            // 
+            // numPhut
+            // 
+            this.numPhut.Location = new System.Drawing.Point(168, 20);
+            this.numPhut.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numPhut.Name = "numPhut";
+            this.numPhut.Size = new System.Drawing.Size(65, 22);
+            this.numPhut.TabIndex = 0;
+            this.numPhut.ValueChanged += new System.EventHandler(this.numPhut_ValueChanged);
+            // 
+            // numGiay
+            // 
+            this.numGiay.Location = new System.Drawing.Point(313, 20);
+            this.numGiay.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numGiay.Name = "numGiay";
+            this.numGiay.Size = new System.Drawing.Size(65, 22);
+            this.numGiay.TabIndex = 0;
+            this.numGiay.ValueChanged += new System.EventHandler(this.numGiay_ValueChanged);
+            // 
+            // btnShutDown
+            // 
+            this.btnShutDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnShutDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnShutDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShutDown.Location = new System.Drawing.Point(31, 62);
+            this.btnShutDown.Name = "btnShutDown";
+            this.btnShutDown.Size = new System.Drawing.Size(111, 36);
+            this.btnShutDown.TabIndex = 1;
+            this.btnShutDown.Text = "Shutdown";
+            this.btnShutDown.UseVisualStyleBackColor = false;
+            this.btnShutDown.Click += new System.EventHandler(this.btnShutDown_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(168, 62);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(111, 36);
+            this.btnRestart.TabIndex = 1;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(313, 62);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(111, 36);
+            this.btnHuy.TabIndex = 1;
+            this.btnHuy.Text = "Hủy lệnh";
+            this.btnHuy.UseVisualStyleBackColor = false;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Giờ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Giây";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phút";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Red;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.Color.Yellow;
+            this.btnThoat.Location = new System.Drawing.Point(168, 119);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(111, 36);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(459, 193);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnShutDown);
+            this.Controls.Add(this.numGiay);
+            this.Controls.Add(this.numPhut);
+            this.Controls.Add(this.numGio);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Set Time";
+            ((System.ComponentModel.ISupportInitialize)(this.numGio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPhut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGiay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numGio;
+        private System.Windows.Forms.NumericUpDown numPhut;
+        private System.Windows.Forms.NumericUpDown numGiay;
+        private System.Windows.Forms.Button btnShutDown;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Timers;
+
+namespace ToolHenGio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadStatusbar();
+        }         
+        
+        StatusBarPanel downtimePanel = new StatusBarPanel();
+        StatusBarPanel barPanel = new StatusBarPanel();
+
+        //Hàm Status
+        private void LoadStatusbar()
+        {
+            StatusBar bar = new StatusBar();
+
+            bar.ShowPanels = true;
+            bar.Panels.Add(barPanel);
+            bar.Panels.Add(downtimePanel);
+
+            downtimePanel.Text = "";
+            barPanel.Text = "Watting...";
+
+            this.Controls.Add(bar);
+        }
+
+        decimal downTime = 0;
+        private void numGiay_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown box = sender as NumericUpDown;
+            if (box.Value >= 60)
+            {
+                numPhut.Value++;
+                box.Value = 0;
+            }
+        }
+
+        private void numPhut_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown box = sender as NumericUpDown;
+            if (box.Value >= 60)
+            {
+                numGio.Value++;
+                box.Value = 0;
+            }
+        }
+       
+        //Hàm tính toán Downtime
+        void calculateDownTime()
+        {
+            downTime = numGiay.Value + numPhut.Value * 60 + numGio.Value * 60 * 60;
+        }
+
+        //shutdown theo command
+        void ShutDown(string command)
+        {
+            System.Diagnostics.Process.Start("shutdown",command);
+        }
+
+        private void btnShutDown_Click(object sender, EventArgs e)
+        {
+
+            calculateDownTime();
+            ShutDown("-s -t "+downTime.ToString());
+            barPanel.Text = "Shutting down...";
+            timer1.Start();
+
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            //ShutDown("-r -t 5");
+            calculateDownTime();
+            ShutDown("-r -t "+downTime.ToString());
+            barPanel.Text = "Restarting...";
+            timer1.Start();
+        }
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            //hủy lệnh là -a
+            ShutDown("-a");
+            barPanel.Text = "Watting...";
+            downtimePanel.Text = "";
+            timer1.Stop();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            downTime--;
+            downtimePanel.Text = downTime.ToString(); 
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
